abb8f69deca75a0805b7a8e00f3030a17cfc33e5
JOÃO VICTOR CARVALHO NICOLAI <joao_nicolai@estudante.sesisenai.org.br>
2024-03-04 09:21:31 -0300

    Class Obj e Carro

---
+++ f1_class.js
@@ -0,0 +1,13 @@
+class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+    }
+}
+
+class Carro extends Obj{
+}
+
